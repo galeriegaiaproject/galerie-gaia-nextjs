d99beada6b34b356a0acd2c107de0573521e1f97
thcolin <th.colin3@gmail.com>
2023-04-15 17:27:45 +0200

    feat: add aws s3 as media provider

--- tina/config.js
+++ tina/config.js
@@ -7,16 +7,15 @@ export default defineConfig({
   branch,
   clientId: 'b7353ecc-6ae7-4629-ac8c-94dca69666a7', // Get this from tina.io
   token: 'e4b5008c49d5df10ab2f345ea78d87e93e2d8f74', // Get this from tina.io
-
+  media: {
+    loadCustomStore: async () => {
+      const pack = await import('next-tinacms-s3')
+      return pack.TinaCloudS3MediaStore
+    },
+  },
   build: {
     outputFolder: 'admin',
     publicFolder: 'public',
-  },
-  media: {
-    tina: {
-      mediaRoot: '',
-      publicFolder: 'public',
-    },
   },
   schema: {
     collections: [
